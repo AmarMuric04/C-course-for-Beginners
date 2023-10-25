bd8bba8c078c88a1602e28d2f5e9a9fb8ad953fb
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-10-25 23:56:54 +0200

    if else vezbanje

--- app-8.c
+++ app-8.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 
+int main()
+{
+
+  // char string[20];
+  // char age[20];
+  // printf("\nEnter your name:");
+  // fgets(string, 20, stdin);
+  // printf("\nEnter your age using letters");
+
+  // fgets(age, 20, stdin);
+  // printf("Your name is %sYour age is %s", string, age);
+
+  int a;
+  int b = 10;
+  printf("Unesi broj");
+  scanf("%d", &a);
+
+  if (a > b)
+    printf("vas broj je veci od %d", b);
+  else
+    printf("Vas broj je manji od %d", b);
+
+  return 0;
+}
